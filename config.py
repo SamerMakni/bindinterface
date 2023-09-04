bca8eb26d508242206f04fc2429abfba3486ab91
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2023-09-04 09:53:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-DATA_POINT = "./data/cidals_user_view.csv"
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
-USERNAME = "cidalsdb.contact@gmail.com"
-PASSWORD = "mxslyjplrluudlgs"
+# config goes here
