00161bf22660638be3f613bcf262b7f05e35b032
SamerMakni <dmc4samer@gmail.com>
2023-10-03 00:10:16 +0200

    fixed contact.py

--- contact.py
+++ contact.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import re
+from Emailer import EmailSender
+
 def is_valid_email(email):
     # Simple email format validation using regex
     pattern = r"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
@@ -21,9 +24,11 @@ def contact():
         if submitted:
             if name and email and subject and message:  
                 if is_valid_email(email):
+                    email_ = EmailSender()
+                    subject_ = f"Contact from {name}, [{email}, {subject}]"
+                    email_.send_email("cidalsdb.contact@gmail.com", subject_, message)
                     st.success("Thank you! Your data access request has been submitted.")
                 else:
                     st.warning("Please enter a valid email address.")
             else:
                 st.warning("Please fill out all the fields.")
-
