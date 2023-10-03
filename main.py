7503ada92313783901f7b7a67041647105962013
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2023-10-03 19:45:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from streamlit_extras.app_logo import add_logo
 
 st.set_page_config(
     page_title="CidalsDB",
-    page_icon="./images/chemistry.png",
+    page_icon="./media/logo_BIND.ico",
     layout="wide",
 )
 
