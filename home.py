87462a53d38ea898668a5b583f937538c88296c2
SamerMakni <dmc4samer@gmail.com>
2023-10-03 02:56:01 +0200

    Improved home page

--- home.py
+++ home.py
@@ -1,6 +1,19 @@
 import streamlit as st
 from PIL import Image
 
+
 def home():
-    image = Image.open('./media/Logo_MEEP.png')
-    st.image(image, width=400)
+    meep = Image.open('./media/Logo_MEEP.png')
+    bind = Image.open('./media/Logo_BIND.png')
+    ipt = Image.open('./media/Logo_IPT.png')
+
+    col1, col2, col3= st.columns([0.2, 0.4, 0.4])
+    with col1:
+        st.image(bind, width=200)
+
+    with col2:
+        st.image(meep, width=400)
+
+    with col3:
+        st.image(ipt, width=200)
+    st.markdown(r"$$\textrm{\large{CidalsDB and its ML-based activity prediction module present a democratized and no-code CADD tool.}}$$")
