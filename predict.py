f00b41a09e3d8ae2745eef1bd00792632688693b
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2023-12-30 01:01:43 +0000

    updated prediction

--- predict.py
+++ predict.py
@@ -148,16 +148,15 @@ def predict():
                 smile = pubchem_id_to_smiles(pub)
                 name = generate_name(smile)
                 st.caption(name)
-                render = makeblock(smile)
                 with col1:
+                    render = makeblock(smile)
                     render_mol(render)
                 progress_text = "Operation in progress. Please wait."
-                with st.spinner(progress_text):
-                    if predict_with_model(smile, f".//models/{option}.pkl") == 1:
-                        with col2:
+                with col2:
+                    with st.spinner(progress_text):
+                        if predict_with_model(smile, f".//models/{option}.pkl") == 1:
                             st.success('Active', icon="✅")
-                    elif predict_with_model(smile, f"./models/{option}.pkl") == 0:
-                        with col2:
+                        elif predict_with_model(smile, f"./models/{option}.pkl") == 0:
                             st.error('Inactive', icon="❌")
             except Exception as e:
                 print(e)
