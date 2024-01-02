6af5f2bd311dea4c9c932c71fa246a57bbf1b087
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2024-01-02 19:56:03 +0000

    more changes to home text

--- about.py
+++ about.py
@@ -115,7 +115,7 @@ def about():
     ## Acknowledgement
     """)
 
-    cola, colb, colc= st.columns([0.2, 0.6, 0.2])
+    cola, colb, colc= st.columns(3)
     with cola:
         st.image(bind, width=300)
     with colb:
@@ -126,10 +126,12 @@ def about():
     st.markdown(r"""
     ### References
     
-1. Harigua-Souiai, E., Oualha, R., Souiai, O., Abdeljaoued-Tej, I. & Guizani, I. Applied machine learning toward drug discovery enhancement: Leishmaniases as a case study. Bioinforma. Biol. Insights 16, 11779322221090349 (2022).
-2. Harigua-Souiai, E. et al. Deep learning algorithms achieved satisfactory predictions when trained on a novel collection of anticoronavirus molecules. Front. Genet. 12, 744170 (2021).
-3. Harigua-Souiai, E., Makni, S., Masmoudi, O., Hamdi, S., Oualha, R., Abdelkrim, Y.Z.,Souiai, O., Guizani, I. CidalsDB: An AI-empowered platform for anti-pathogen therapeutics research. Submitted.
-                
+[^1^]: Harigua-Souiai, E., Oualha, R., Souiai, O., Abdeljaoued-Tej, I. & Guizani, I. (2022). Applied machine learning toward drug discovery enhancement: Leishmaniases as a case study. Bioinforma. Biol. Insights 16, 11779322221090349.
+
+[^2^]: Harigua-Souiai, E. et al. (2021). Deep learning algorithms achieved satisfactory predictions when trained on a novel collection of anticoronavirus molecules. Front. Genet. 12, 744170.
+
+[^3^]: Harigua-Souiai, E., Makni, S., Masmoudi, O., Hamdi, S., Oualha, R., Abdelkrim, Y.Z., Souiai, O., Guizani, I. CidalsDB: An AI-empowered platform for anti-pathogen therapeutics research. Submitted.
+    
                 """)
     
     
--- home.py
+++ home.py
@@ -20,13 +20,6 @@ def home():
     CidalsDB is an open resource on anti-pathogen molecules that provides a chemical similarity-based browsing function and an AI-based prediction tool for the 'cidal' effect of chemical compounds against pathogens. These include Leishmania parasites[^1^] and Coronaviruses (SARS-Cov, SARS-Cov-2, MERS)[^2^].
 
 CidalsDB serves as an evolutive platform for democratized and no-code Computer-Aided Drug Discovery (CADD)[^3^]. We are committed to continuous improvement, actively incorporating additional pathogens, datasets, and AI predictive models.
-
-## References
-[^1^]: Harigua-Souiai, E., Oualha, R., Souiai, O., Abdeljaoued-Tej, I. & Guizani, I. (2022). Applied machine learning toward drug discovery enhancement: Leishmaniases as a case study. Bioinforma. Biol. Insights 16, 11779322221090349.
-
-[^2^]: Harigua-Souiai, E. et al. (2021). Deep learning algorithms achieved satisfactory predictions when trained on a novel collection of anticoronavirus molecules. Front. Genet. 12, 744170.
-
-[^3^]: Harigua-Souiai, E., Makni, S., Masmoudi, O., Hamdi, S., Oualha, R., Abdelkrim, Y.Z., Souiai, O., Guizani, I. CidalsDB: An AI-empowered platform for anti-pathogen therapeutics research. Submitted.
-    """)
+ """)
 
     
