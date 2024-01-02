c5e08d623be4c4a82350fcaae7f00f3915cc731c
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2024-01-02 20:24:01 +0000

    more changes to home text

--- home.py
+++ home.py
@@ -22,11 +22,4 @@ CidalsDB is an open resource on anti-pathogen molecules that provides a chemical
 CidalsDB serves as an evolutive platform for democratized and no-code Computer-Aided Drug Discovery (CADD)[**3**]. We would like to acknowledge the following contributions:
 """)
 
-    st.markdown(r"""
-    ## References
-1. Harigua-Souiai, E., Oualha, R., Souiai, O., Abdeljaoued-Tej, I. & Guizani, I. (2022). Applied machine learning toward drug discovery enhancement: Leishmaniases as a case study. Bioinforma. Biol. Insights 16, 11779322221090349.
-
-2. Harigua-Souiai, E. et al. (2021). Deep learning algorithms achieved satisfactory predictions when trained on a novel collection of anticoronavirus molecules. Front. Genet. 12, 744170.
-
-3. Harigua-Souiai, E., Makni, S., Masmoudi, O., Hamdi, S., Oualha, R., Abdelkrim, Y.Z., Souiai, O., Guizani, I. CidalsDB: An AI-empowered platform for anti-pathogen therapeutics research. Submitted.
-    """)
+    
