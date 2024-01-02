1c811153c4bf3dbf8cec4f55043d282686a73409
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2024-01-02 19:53:04 +0000

    more changes to home text

--- about.py
+++ about.py
@@ -8,6 +8,10 @@ import base64
 df = pd.read_csv("./data/cidals_user_view.csv")
 
 text = lorem.paragraph()
+meep = Image.open('./media/Logo_MEEP.png')
+bind = Image.open('./media/logo_BIND.png')
+ipt = Image.open('./media/logo_IPT.png')
+
 def about():
     st.title("About CidalsDB")
     st.header("Datasets")
@@ -92,9 +96,10 @@ def about():
             The process of data encoding involves transforming the SMILES representations from our molecule dataset into molecular fingerprints. This transformation allows us to extract features from each molecule and represent them in a suitable manner for machine learning algorithms.
             We opted to use [RDKitDescriptors](https://www.rdkit.org/docs/source/rdkit.Chem.Descriptors.html), which are binary vectors with a size we determined to be 2048.
 
-            ### Training
+            ### Predictive models
+            We trained and optimized ML and DL algorithms on the content of the CidalsDB, namely the RF model on the Leishmania dataset and the GCN model on the Coronaviruses dataset.
 
-            The dataset used for training is a BioAssay available on [PubChem](https://pubchem.ncbi.nlm.nih.gov/bioassay/1063)(Leishmania major promastigote HTS).  Our approach to data splitting is the 80/20 split, where the dataset is divided into an 80\% training set 
+            The dataset used for Leishmania is a BioAssay available on [PubChem](https://pubchem.ncbi.nlm.nih.gov/bioassay/1063)(Leishmania major promastigote HTS).  Our approach to data splitting is the 80/20 split, where the dataset is divided into an 80\% training set 
             $$ D_{train} $$  and a 20\%  test set $$ D_{test} $$ , $$len(D_{train}) = 156,938 $$ and  $$len(D_{test}) = 39,235 $$
             To address the problem of class imbalance present in our dataset, we applied Random Oversampling to the training set, changing the ratio between the two classes from 1:9 to 1:1.""")
 
@@ -102,4 +107,29 @@ def about():
     left_co, cent_co,last_co = st.columns(3)
     with cent_co:
         st.image("https://i.ibb.co/9Y8ppQ6/model-ml-drawio.png", output_format = "png", caption="Pipeline of Traning and Predictions", width=550)
+    
+
+
+    st.markdown(r"""
+    ## Acknowledgement
+    """)
+
+    cola, colb, colc= st.columns([0.2, 0.6, 0.2])
+    with cola:
+        st.image(bind, width=300)
+    with colb:
+        st.image(meep, width=300)
+    with colb:
+        st.image(ipt, width=300)
+
+    st.markdown(r"""
+    ### References
+    
+1. Harigua-Souiai, E., Oualha, R., Souiai, O., Abdeljaoued-Tej, I. & Guizani, I. Applied machine learning toward drug discovery enhancement: Leishmaniases as a case study. Bioinforma. Biol. Insights 16, 11779322221090349 (2022).
+2. Harigua-Souiai, E. et al. Deep learning algorithms achieved satisfactory predictions when trained on a novel collection of anticoronavirus molecules. Front. Genet. 12, 744170 (2021).
+3. Harigua-Souiai, E., Makni, S., Masmoudi, O., Hamdi, S., Oualha, R., Abdelkrim, Y.Z.,Souiai, O., Guizani, I. CidalsDB: An AI-empowered platform for anti-pathogen therapeutics research. Submitted.
+                
+                """)
+    
+    
     
--- home.py
+++ home.py
@@ -16,14 +16,17 @@ def home():
         pass
     with colb:
         pass
-    st.markdown("""
-    CidalsDB is an open resource on anti-pathogen molecules that offers a chemical similarity-based browsing function and an AI-based prediction tool of the 'cidal' effect of chemical compounds against pathogens, such as Leishmania parasites1 and Coronaviruses (SARS-Cov, SARS-Cov-2, MERS)2.
+    st.markdown(r"""
+    CidalsDB is an open resource on anti-pathogen molecules that provides a chemical similarity-based browsing function and an AI-based prediction tool for the 'cidal' effect of chemical compounds against pathogens. These include Leishmania parasites[^1^] and Coronaviruses (SARS-Cov, SARS-Cov-2, MERS)[^2^].
 
-CidalsDB is an evolutive platform for democratized and no-code Computer-Aided Drug Discovery (CADD)3, and we work continuously on including further pathogens, datasets and AI predictive models.
+CidalsDB serves as an evolutive platform for democratized and no-code Computer-Aided Drug Discovery (CADD)[^3^]. We are committed to continuous improvement, actively incorporating additional pathogens, datasets, and AI predictive models.
 
-1. Harigua-Souiai, E., Oualha, R., Souiai, O., Abdeljaoued-Tej, I. & Guizani, I. Applied machine learning toward drug discovery enhancement: Leishmaniases as a case study. Bioinforma. Biol. Insights 16, 11779322221090349 (2022).
-2. Harigua-Souiai, E. et al. Deep learning algorithms achieved satisfactory predictions when trained on a novel collection of anticoronavirus molecules. Front. Genet. 12, 744170 (2021).
-3. Harigua-Souiai, E., Makni, S., Masmoudi, O., Hamdi, S., Oualha, R., Abdelkrim, Y.Z.,Souiai, O., Guizani, I. CidalsDB: An AI-empowered platform for anti-pathogen therapeutics research. Submitted.
-    
-    
+## References
+[^1^]: Harigua-Souiai, E., Oualha, R., Souiai, O., Abdeljaoued-Tej, I. & Guizani, I. (2022). Applied machine learning toward drug discovery enhancement: Leishmaniases as a case study. Bioinforma. Biol. Insights 16, 11779322221090349.
+
+[^2^]: Harigua-Souiai, E. et al. (2021). Deep learning algorithms achieved satisfactory predictions when trained on a novel collection of anticoronavirus molecules. Front. Genet. 12, 744170.
+
+[^3^]: Harigua-Souiai, E., Makni, S., Masmoudi, O., Hamdi, S., Oualha, R., Abdelkrim, Y.Z., Souiai, O., Guizani, I. CidalsDB: An AI-empowered platform for anti-pathogen therapeutics research. Submitted.
     """)
+
+    
