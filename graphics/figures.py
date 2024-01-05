f66c41ed9f30a2c883a8bd295f99020a64adfa3d
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2024-01-05 18:51:16 +0000

    chart changes

--- graphics/figures.py
+++ graphics/figures.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("./data/cidals_final.csv")
+df = pd.read_csv("./data/cidals_final_2.csv")
 df_full = pd.read_csv("./data/cidals_full_view.csv")
 
 unique_values = df["Biological Activity"]
