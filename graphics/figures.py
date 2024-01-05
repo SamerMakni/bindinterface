efd4766e7182cce6b12400ed8f5abe462d663f08
Samer Makni <73069842+SamerMakni@users.noreply.github.com>
2024-01-05 19:42:37 +0000

    changes to graph

--- graphics/figures.py
+++ graphics/figures.py
@@ -7,32 +7,16 @@ df_unique_smiles = df.drop_duplicates(subset="SMILES")
 unique_values = df_unique_smiles["Biological Activity"]
 value_counts = pd.Series(unique_values).value_counts()
 def pie():
-    # colors = ["#5454b3", "#8CC589"]
-    # fig = px.pie(
-    #     data_frame=df,
-    #     values=value_counts,
-    #     names=value_counts.index,
-    #     title="Active vs. Inactive Distribution (Leishmania dataset)",
-    #     color=value_counts.index,
-    #     color_discrete_map={"Active": colors[0], "Inactive": colors[1]}
-    # )
-    # fig.update_layout(width=500,height=350)
-    total_compounds = len(df_full)
-    unique_compounds = len(df_unique_smiles)
-    active_compounds = df_full["Biological Activity"].eq("Active").sum()
-    inactive_compounds = df_full["Biological Activity"].eq("Inactive").sum()
-
-    # Create a sunburst chart
-    fig = px.sunburst(
-        names=["Total Compounds", "Unique Compounds", "Active", "Inactive"],
-        parents=["", "Total Compounds", "Total Compounds", "Total Compounds"],
-        values=[total_compounds, unique_compounds, active_compounds, inactive_compounds],
-        title="Sunburst Chart of Compounds Distribution",
-        color=["Total Compounds", "Unique Compounds", "Active", "Inactive"],
-        color_discrete_map={"Total Compounds": "#5454b3", "Unique Compounds": "#8CC589", "Active": "#5454b3", "Inactive": "#8CC589"}
+    colors = ["#5454b3", "#8CC589"]
+    fig = px.pie(
+        data_frame=df,
+        values=value_counts,
+        names=value_counts.index,
+        title="Active vs. Inactive Distribution (Leishmania dataset)",
+        color=value_counts.index,
+        color_discrete_map={"Active": colors[0], "Inactive": colors[1]}
     )
-
-    fig.update_layout(width=700, height=500)
+    fig.update_layout(width=500,height=350)
     return fig
 
 def bar():
